79ee08e5a3f5cfab06a0725477909a8d189f14ff
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-07 07:43:52 -0300

    creación de la ruta /detail/:id

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const postProduct = require("../controllers/Product/postProduct");
+const postProduct = require("../controllers/product/postProduct");
 const postProductByPk = require("../controllers/Product/deleteProductByPk");
 const { getProduct } = require("../controllers/Product/getProduct");
+const { getProductByPk } = require("../controllers/product/getProductByPk");
 
 router.post("/product", postProduct);
 router.get("/product", getProduct);
+router.get("/detail/:id", getProductByPk);
 
 router.delete("/product/:productId", postProductByPk);
 
