409788ae4f8f162d861445f1e0d18df1f81ec642
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-10 02:50:09 -0300

    se saca el console log de app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
-  console.log("ruta recibida" + req.url);
 });
 app.use(cors());
 app.use(express.json());
