9922ad8f32ce2c835bb98a1ec059e41679fe7b50
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-18 20:12:22 -0300

    actualización

--- src/controllers/product/postProduct.js
+++ src/controllers/product/postProduct.js
@@ -7,43 +7,42 @@ const postProduct = async (req, res) => {
   try {
     const { title, description, brand, color, category, subCategory, sizes, gender, price, discount, images } =
       req.body;
-    console.log(title, "post product");
 
-    // if (title && description && brand && price && category && sizes.length && images.length) {
-    //   const isThisAlreadyCreated = await Product.findOne({
-    //     where: {
-    //       title: title.toUpperCase(),
-    //       description: description.toUpperCase(),
-    //       brand: brand.toUpperCase(),
-    //       category: category.toUpperCase(),
-    //       subCategory: subCategory?.toUpperCase(),
-    //       gender: gender.toUpperCase(),
-    //       price,
-    //       discount,
-    //     },
-    //   });
+    if (title && description && brand && price && category && sizes.length && images.length) {
+      const isThisAlreadyCreated = await Product.findOne({
+        where: {
+          title: title.toUpperCase(),
+          description: description.toUpperCase(),
+          brand: brand.toUpperCase(),
+          category: category.toUpperCase(),
+          subCategory: subCategory?.toUpperCase(),
+          gender: gender.toUpperCase(),
+          price,
+          discount,
+        },
+      });
 
-    // Si no se encuentra el producto en la base de datos, se crea la instancia.
-    // if (!isThisAlreadyCreated) {
-    const response = await createProduct({
-      title,
-      description,
-      category,
-      subCategory,
-      brand,
-      color,
-      sizes,
-      price,
-      discount,
-      images,
-      gender,
-    });
+      // Si no se encuentra el producto en la base de datos, se crea la instancia.
+      if (!isThisAlreadyCreated) {
+        const response = await createProduct({
+          title,
+          description,
+          category,
+          subCategory,
+          brand,
+          color,
+          sizes,
+          price,
+          discount,
+          images,
+          gender,
+        });
 
-    return res.status(201).json(response);
-    // } else throw Error("El producto ya existe");
-    // } else throw Error("Faltan datos");
+        return res.status(201).json(response);
+      } else throw Error("El producto ya existe");
+    } else throw Error("Faltan datos");
   } catch (error) {
-    res.status(500).json("estoy en el error post product");
+    res.status(500).json({ error: error.message });
   }
 };
 
