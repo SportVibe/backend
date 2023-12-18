ad15e12dc183bb728bff29e4f2837eb59a5a1a0e
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-18 20:22:29 -0300

    prueba

--- src/app.js
+++ src/app.js
@@ -26,12 +26,12 @@ app.use((req, res, next) => {
 app.use("/", routes);
 
 // Error catching endware.
-app.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
+// app.use((err, req, res, next) => {
+//   // eslint-disable-line no-unused-vars
+//   const status = err.status || 500;
+//   const message = err.message || err;
+//   console.error(err);
+//   res.status(status).send(message);
+// });
 
 module.exports = app;
