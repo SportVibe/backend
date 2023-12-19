46e41d2947887eb59c23eb523c7eae76ff8dec36
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-19 07:16:48 -0300

    PRUEBA

--- src/handlers/Product/createProduct.js
+++ src/handlers/Product/createProduct.js
@@ -31,7 +31,7 @@ const createProduct = async ({
         where: { name: sizeInfo.size },
       });
       console.log({ currentProduct, size, sizeInfo });
-      await Stock.create({
+      await Stock.findOrCreate({
         product_id: currentProduct.id,
         size_id: size.id,
         quantity: sizeInfo.stock,
