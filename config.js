eadcce33b49c541feb292abe149cf46a4bb2ed1f
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-22 02:05:13 -0300

    prueba

--- config.js
+++ config.js
@@ -15,7 +15,8 @@ const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "ecommerce";
 const DB_PORT = process.env.DB_PORT || 5432;
-const DB_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+// const DB_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const DB_URL = "postgresql://postgres:a-3E4B4-3F-eEgF3Bd-cbfeDf1AE526a@monorail.proxy.rlwy.net:52992/railway";
 
 module.exports = {
   SECRETKEY,
