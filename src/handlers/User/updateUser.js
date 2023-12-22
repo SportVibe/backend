e1e2568e7470aad7992d87f7e65f9d028428a1c0
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-22 02:56:40 -0300

    se modifica updateUser

--- src/handlers/User/updateUser.js
+++ src/handlers/User/updateUser.js
@@ -1,16 +1,9 @@
 const { User } = require("../../db");
-const bcrypt = require("bcrypt");
 
 const updateUser = async (user) => {
   try {
-    let hashedPassword = null;
-    const external = await User.findByPk(user.id);
-    if (!external.externalSignIn) {
-      hashedPassword = await bcrypt.hash(user.password, 10);
-    }
-
     const userUpdated = await User.update(
-      { ...user, password: hashedPassword },
+      { ...user },
       {
         where: {
           id: user.id,
