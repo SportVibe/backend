884551c25ae8a1845a76233886b17779fd66cb92
Daniel Amaya <danijcdm.com@gmail.com>
2023-12-25 08:47:10 -0500

    delete-terminado

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const SECRETKEY = "sportvibe";
 const clientID = "";
 
 const DB_USER = process.env.DB_USER || "postgres";
-const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
+const DB_PASSWORD = process.env.DB_PASSWORD || "123456";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "ecommerce";
 const DB_PORT = process.env.DB_PORT || 5432;
