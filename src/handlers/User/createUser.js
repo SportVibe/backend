be306bd5640a23905818b40bede01b99e2116d7a
sport <sportvibe07@gmail.com>
2023-12-26 05:30:07 -0300

    corrección valores nullos en el createUser

--- src/handlers/User/createUser.js
+++ src/handlers/User/createUser.js
@@ -33,18 +33,16 @@ const createUser = async (req, res) => {
           throw Error("El correo electrónico ya está registrado");
         }
         else {
-          console.log('tukis');
           const hashedPassword = await bcrypt.hash(password, 10);
-          console.log(hashedPassword);
           const newUser = await User.create({
             firstName: firstName && firstName.toUpperCase(),
-            lastName: lastName && lastName.toUpperCase(),
-            phoneNumber,
-            address: address && address.toUpperCase(),
-            city: city && city.toUpperCase(),
-            country: country && country.toUpperCase(),
-            zipCode,
-            email,
+            lastName: lastName ? lastName.toUpperCase(): null,
+            phoneNumber: phoneNumber ? phoneNumber: null,
+            address: address ? address.toUpperCase(): null,
+            city: city ? city.toUpperCase(): null,
+            country: country ? country.toUpperCase(): null,
+            zipCode: zipCode ? zipCode: null,
+            email: email ? email: null,
             password: hashedPassword,
             rol,
             image: userImage,
