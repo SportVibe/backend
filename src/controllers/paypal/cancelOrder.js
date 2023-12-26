9dc7c1fae782dc2d2a7c2895c3dab51ceedbce4b
VijarraM <121908120+VijarraM@users.noreply.github.com>
2023-12-25 21:43:30 -0300

    se agrega try catch a los controllers de paypal

--- src/controllers/paypal/cancelOrder.js
+++ src/controllers/paypal/cancelOrder.js
@@ -1,4 +1,10 @@
 const cancelOrder = async (req, res) => {
-  res.send("se cancelo el pago :(");
+  try {
+    res.send("¡Se canceló el pago!");
+  } catch (error) {
+    console.error("Error al cancelar el pago:", error);
+    return res.status(500).json({ error: "Error Interno del Servidor" });
+  }
 };
+
 module.exports = cancelOrder;
--- src/controllers/paypal/captureOrder.js
+++ src/controllers/paypal/captureOrder.js
@@ -2,22 +2,27 @@ const { PAYPAL_URL, PAYPAL_CLIENT, PAYPAL_SECRET_KEY } = require("../../../confi
 const axios = require("axios");
 
 const captureOrder = async (req, res) => {
-  const { token } = req.query;
+  try {
+    const { token } = req.query;
 
-  const response = await axios.post(
-    `${PAYPAL_URL}/v2/checkout/orders/${token}/capture`,
-    {},
-    {
-      auth: {
-        username: PAYPAL_CLIENT,
-        password: PAYPAL_SECRET_KEY,
-      },
-    }
-  );
+    const response = await axios.post(
+      `${PAYPAL_URL}/v2/checkout/orders/${token}/capture`,
+      {},
+      {
+        auth: {
+          username: PAYPAL_CLIENT,
+          password: PAYPAL_SECRET_KEY,
+        },
+      }
+    );
 
-  console.log(response.data);
+    console.log(response.data);
 
-  return res.send("pagado!");
+    return res.send("¡Pagado!");
+  } catch (error) {
+    console.error("Error al capturar la orden:", error);
+    return res.status(500).json({ error: "Error Interno del Servidor" });
+  }
 };
 
 module.exports = captureOrder;
