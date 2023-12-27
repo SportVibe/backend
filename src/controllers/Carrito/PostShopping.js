305e48ce2aff5f2ad295d95150af31000298124f
sport <sportvibe07@gmail.com>
2023-12-27 04:06:00 -0300

    detalles del shoppingCart

--- src/controllers/Carrito/PostShopping.js
+++ src/controllers/Carrito/PostShopping.js
@@ -2,27 +2,25 @@ const { ShoppingCart, User } = require("../../db");
 
 const postShopping = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { id } = req.query;
 
-    if (!userId) {
+    if (!id) {
       return res.status(401).json({ error: "Falta id del usuario" });
     }
 
-    const user = await User.findByPk(userId);
+    const user = await User.findByPk(id);
 
     if (!user) {
       return res.status(401).json({ error: "No existe el Usuario en la BD" });
     }
 
     const [newCart, created] = await ShoppingCart.findOrCreate({
-      where: { UserId: userId },
-      defaults: {
-        total: 0,
-      },
+      where: { UserId: id },
     });
 
     return res.status(200).json(newCart);
   } catch (error) {
+    console.error({ error: error.message });
     res.status(400).json({ error: error.message });
   }
 };
