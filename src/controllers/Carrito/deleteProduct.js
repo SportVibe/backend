31c017d77f3ca5a11151615a09f81de668b1c933
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-01-02 03:03:24 -0300

    actualizacion de delete product del carrito

--- src/controllers/Carrito/deleteProduct.js
+++ src/controllers/Carrito/deleteProduct.js
@@ -1,19 +1,19 @@
-const deleteProductHandler = require('../../handlers/Carrito/deleteProduct');
+const deleteProductHandler = require("../../handlers/Carrito/deleteProduct");
 
 const deleteProduct = async (req, res) => {
-    try {
-      const { shoppingId, productId } = req.body;
-  
-      const result = await deleteProductHandler(shoppingId, productId);
-  
-      if (result.message) {
-        return res.status(400).json({ error: result.message });
-      }
-  
-      return res.status(200).json(result.updatedCart);
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
+  try {
+    const { shoppingId, productId } = req.body;
+
+    const result = await deleteProductHandler(shoppingId, productId);
+
+    if (result.message === "Producto eliminado del carrito") {
+      return res.status(200).json(result.message);
+    } else {
+      return res.status(401).json(result);
     }
-  };
-  
-  module.exports = deleteProduct;
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = deleteProduct;
