9d6bdf018fdacc0892e3239a990f6ecf1906387d
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-01-06 15:33:15 -0300

    se permite como valor null en description

--- src/models/Reviews.js
+++ src/models/Reviews.js
@@ -16,7 +16,7 @@ module.exports = (sequelize) => {
       },
       description: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       status: {
         type: DataTypes.ENUM("pending", "accepted", "rejected"),
