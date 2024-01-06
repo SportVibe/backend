f51205634e558e4302a623e9ed7ce11769dc988d
sport <sportvibe07@gmail.com>
2024-01-05 21:25:36 -0300

    update luca

--- src/controllers/User/postRegister.js
+++ src/controllers/User/postRegister.js
@@ -4,24 +4,7 @@ const postRegister = async (req, res) => {
   try {
     const newUser = await createUser(req, res);
 
-    res.status(201).json({
-      message: `Usuario ${newUser.firstName} registrado exitosamente`,
-      user: {
-        id: newUser.id,
-        firstName: newUser.firstName,
-        lastName: newUser.lastName,
-        image: newUser.image,
-        email: newUser.email,
-        address: newUser.address,
-        city: newUser.city,
-        country: newUser.country,
-        zipCode: newUser.zipCode,
-        email: newUser.email,
-        phoneNumber: newUser.phoneNumber,
-        sendMailsActive: newUser.sendMailsActive,
-        externalSignIn: newUser.externalSignIn
-      }
-    });
+    res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json({ error: `Error en el registro:, ${error.message}` });
   }
