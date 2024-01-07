abe2ce73180e211fedad87b5ce3e03821c02598c
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-01-07 02:22:44 -0300

    se modifica get reviews para dar mas opciones de búsquedas

--- src/controllers/reviews/getReview.js
+++ src/controllers/reviews/getReview.js
@@ -2,9 +2,9 @@ const { Reviews } = require("../../db");
 
 const getReview = async (req, res) => {
   try {
-    const { status, productId } = req.query;
+    const { status, productId, reviewId, userId } = req.query;
 
-    if (!status && !productId) {
+    if (!status && !productId && !reviewId && !userId) {
       const reviews = await Reviews.findAll();
 
       return res.status(200).json({
@@ -13,7 +13,7 @@ const getReview = async (req, res) => {
       });
     }
 
-    if (status && !productId) {
+    if (status && !productId && !reviewId && !userId) {
       const reviews = await Reviews.findAll({
         where: {
           status: status,
@@ -29,7 +29,7 @@ const getReview = async (req, res) => {
       });
     }
 
-    if (!status && productId) {
+    if (!status && productId && !reviewId && !userId) {
       const reviews = await Reviews.findAll({
         where: {
           ProductId: productId,
@@ -37,7 +37,40 @@ const getReview = async (req, res) => {
       });
 
       if (!reviews.length) {
-        return res.status(400).json({ message: `No se encuentran reseñas con ID: ${productId}.` });
+        return res.status(400).json({ message: `No se encuentran reseñas con productId: ${productId}.` });
+      }
+      return res.status(200).json({
+        message: "OK",
+        data: reviews,
+      });
+    }
+    if (reviewId && !productId && !status && !userId) {
+      const reviews = await Reviews.findAll({
+        where: {
+          id: reviewId,
+        },
+      });
+
+      if (!reviews.length) {
+        return res.status(400).json({ message: `No se encuentran reseñas con ID: ${reviewId}.` });
+      }
+      return res.status(200).json({
+        message: "OK",
+        data: reviews,
+      });
+    }
+    if (!reviewId && productId && !status && userId) {
+      const reviews = await Reviews.findAll({
+        where: {
+          ProductId: productId,
+          UserId: userId,
+        },
+      });
+
+      if (!reviews.length) {
+        return res.status(400).json({
+          message: `No se encuentran reseñas del UserID: ${userId}, que coincidan con el productID: ${productId}`,
+        });
       }
       return res.status(200).json({
         message: "OK",
