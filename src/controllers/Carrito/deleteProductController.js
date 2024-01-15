4cc23e14c982dabc40094ab34aaa9e0ce9daa062
brianrojas <briaan.dc@gmail.com>
2024-01-15 20:55:26 -0300

    Modificacion de ruta deleteproduct

--- src/controllers/Carrito/deleteProductController.js
+++ src/controllers/Carrito/deleteProductController.js
@@ -2,9 +2,21 @@ const deleteProductHandler = require("../../handlers/Carrito/deleteProduct");
 
 const deleteProduct = async (req, res) => {
   try {
-    const { shoppingId, productId } = req.body;
+    const { idUser, products } = req.body;
 
-    const result = await deleteProductHandler(shoppingId, productId);
+    if (!idUser) {
+      return res.status(401).json({ error: "Falta id del usuario." });
+    }
+
+    if (products.length === 0) {
+      return res.status(401).json({ error: "El carrito esta vacio." });
+    }
+
+    if (products[0].quantity > 1) {
+      return res.status(401).json({ error: "Recuerde que el metodo delete se utiliza solo en el ultimo producto." });
+    }
+
+    const result = await deleteProductHandler(idUser, products);
 
     if (result.message === "Producto eliminado del carrito") {
       return res.status(200).json(result);
--- src/handlers/Carrito/deleteProduct.js
+++ src/handlers/Carrito/deleteProduct.js
@@ -1,48 +1,65 @@
 const { ShoppingCart, Cart_Product } = require("../../db");
+const { moldeRespuesta } = require("../../handlers/Carrito/PutProduct");
 const { Op } = require("sequelize");
 
-const deleteProduct = async (shoppingId, productId) => {
+const deleteProduct = async (idUser, products) => {
   try {
-    const cart = await ShoppingCart.findByPk(shoppingId);
-
-    if (!cart) {
-      return { message: "Carrito no encontrado" };
+    const cartUser = await ShoppingCart.findOne({
+      where: { UserId: idUser },
+    });
+    //Si no se encuentra un carrito asignado al usuario se da aviso
+    if (!cartUser) {
+      return { message: "no se encontro carrito con ese UserId" };
     }
 
-    const deletedProduct = await Cart_Product.findOne({
+    const existsInTheCart = await Cart_Product.findOne({
       where: {
-        [Op.and]: [{ ShoppingCartId: cart.id }, { ProductId: productId }],
+        [Op.and]: [{ ShoppingCartId: cartUser.id }, { ProductId: products[0].id }],
       },
     });
 
-    if (!deletedProduct) {
+    if (!existsInTheCart) {
       return { message: "Producto no encontrado en el carrito" };
+    } else {
+      await Cart_Product.destroy({
+        where: {
+          ShoppingCartId: shoppingId,
+          ProductId: productId,
+        },
+      });
     }
 
-    await Cart_Product.destroy({
+    const carrito = await Cart_Product.findAll({
       where: {
-        ShoppingCartId: shoppingId,
-        ProductId: productId,
+        ShoppingCartId: cartUser.id,
       },
     });
-
-    const productsInCart = await Cart_Product.findAll({
-      where: { ShoppingCartId: shoppingId },
-    });
-
-    const subTotales = productsInCart.map((producto) => {
+    const totalBruto = carrito.map((producto) => {
       return {
         subtotal: producto.dataValues.subtotal,
       };
     });
-    const total = subTotales.reduce((acumulador, subtotal) => {
+
+    const total = totalBruto.reduce((acumulador, subtotal) => {
       const subtotalNumero = parseFloat(subtotal.subtotal);
       return acumulador + subtotalNumero;
     }, 0);
 
-    await ShoppingCart.update({ total: total }, { where: { id: shoppingId } });
+    await ShoppingCart.update(
+      { total: total },
+      {
+        where: { UserId: idUser },
+      }
+    );
+    const productos = await Promise.all(carrito.map(({ ProductId, detalle }) => moldeRespuesta(ProductId, detalle)));
 
-    return { message: "Producto eliminado del carrito", total: total };
+    const currentCart = productos.flat(Infinity);
+
+    return {
+      message: "Producto eliminado del carrito",
+      total: total,
+      products: currentCart,
+    };
   } catch (error) {
     return { message: error.message };
   }
